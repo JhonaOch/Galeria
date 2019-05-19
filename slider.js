5ce8d5333414822e766cd308b293ac7012db8aa8
JonnathanOchoa <UPSJhona@users.noreply.github.com>
2019-05-19 03:17:00 -0500

    Cambio de imagenes

---
+++ slider.js
@@ -0,0 +1,33 @@
+let img = [];
+var indice = 0
+var slid
+img[0] = 'imagenes/1.jpg'
+img[1] = 'imagenes/2.jpg'
+img[2] = 'imagenes/3.jpg'
+img[3] = 'imagenes/4.jpg'
+img[4] = 'imagenes/5.jpg'
+img[5] = 'imagenes/6.jpg'
+img[6] = 'imagenes/7.jpg'
+img[7] = 'imagenes/8.jpg'
+img[8] = 'imagenes/9.jpg'
+img[9] = 'imagenes/10.jpg'
+img[10] = 'imagenes/11.jpg'
+
+function cambiarImagen(int) {
+    if (int == 1) {
+        if (indice < img.length - 1) {
+            indice++
+        } else {
+            indice = 0
+        }
+    } else {
+        if (indice > 0) {
+            indice--
+        } else {
+            indice = img.length - 1
+        }
+    }
+    document.getElementById("imagenes").src = img[indice]
+}
+
+
